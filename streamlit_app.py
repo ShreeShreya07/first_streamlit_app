09c5c9cf948cf82cf4605fc87b6d6d4eb8a8d9fd
S. Shreya Shree <106065040+ShreeShreya07@users.noreply.github.com>
2022-05-23 14:29:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.title('Breakfast Menu')
-streamlit('Omega 3 and blueberry oatmeal')
+streamlit.title ('Omega 3 and blueberry oatmeal')
